20e8eef28866394dec68b1c7ad291ac36b639f3b
EsseivaN <nicolas.esseiva@gmail.com>
2019-03-16 17:44:47 +0100

    update

--- PictureSelecter/Properties/AssemblyInfo.cs
+++ PictureSelecter/Properties/AssemblyInfo.cs
@@ -19,6 +19,9 @@ using System.Runtime.InteropServices;
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("3a24ec05-3a28-4a69-ab5d-000000000006")]
+
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
